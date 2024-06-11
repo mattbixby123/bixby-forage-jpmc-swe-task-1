82bd88ae2a65957c6c3fbc0e0346507ed1e3424f
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-06-11 14:10:41 -0400

    first task complete + test assertions completed + tests added for getRatio function

--- client3.py
+++ client3.py
@@ -31,18 +31,21 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
+    """ ------------- Updated ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    """ ------------- Updated ------------- """
+    if price_b == 0:
+            # when the price b is zero this will avoid a ZeroDivisionError
+            return
+    return price_a / price_b
 
 
 # Main
@@ -51,9 +54,15 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        """ ----------- Updated --------------- """
+        # created a prices dictionary to store the stock prices, where the key : stock name and the value : price.
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+
+            # specify key and retrieve a value from the dictionary
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        # last we use the prices dictionary to pass the correct values into the getRation function
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -7,18 +7,50 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    """ ------------ Assertions added below ------------ """
+    for quote in quotes:
+       stock, bid_price, ask_price, price = getDataPoint(quote)
+       self.assertEqual(stock, quote['stock'])
+       self.assertEqual(bid_price, quote['top_bid']['price'])
+       self.assertEqual(ask_price, quote['top_ask']['price'])
+       self.assertEqual(price, (quote['top_bid']['price'] + quote['top_ask']['price']) / 2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    """ ------------ Assertions added below ------------ """
+    for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            self.assertEqual(stock, quote['stock'])
+            self.assertEqual(bid_price, quote['top_bid']['price'])
+            self.assertEqual(ask_price, quote['top_ask']['price'])
+            self.assertEqual(price, (quote['top_bid']['price'] + quote['top_ask']['price']) / 2)
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_positiveNumbers(self):
+        self.assertEqual(getRatio(10, 2), 5)
+        self.assertEqual(getRatio(15, 3), 5)
+        self.assertEqual(getRatio(20, 4), 5)
 
+  def test_getRatio_negativeNumbers(self):
+        self.assertEqual(getRatio(-10, 2), -5)
+        self.assertEqual(getRatio(10, -2), -5)
+        self.assertEqual(getRatio(-10, -2), 5)
+
+  def test_getRatio_zeroDenominator(self):
+        self.assertIsNone(getRatio(10, 0))
+        self.assertIsNone(getRatio(0, 0))
+
+  def test_getRatio_floats(self):
+        self.assertAlmostEqual(getRatio(10.5, 2.5), 4.2)
+        self.assertAlmostEqual(getRatio(7.5, 2.5), 3.0)
+
+  def test_getRatio_largeNumbers(self):
+        self.assertAlmostEqual(getRatio(1e10, 2), 5e9)
+        self.assertAlmostEqual(getRatio(1e10, 1e-10), 1e20)
 
 
 if __name__ == '__main__':
